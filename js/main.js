c9cd135736e8b8b190f6218c808881a976609d3e
jeminic <orowuadominic@gmail.com>
2020-07-11 15:00:40 +0100

    LatestChanges

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+document.getElementById("defaultOpen").click();
+
+
 $(document).ready(function() {
     $('#example').DataTable({
 
@@ -77,5 +80,112 @@ function tab(evt, tabName) {
 
    
   }
+
+  // Get the element with id="defaultOpen" and click on it
+
+
+$(document).ready(function() {
+    $('#example').DataTable({
+        columnDefs: [{
+            orderable: false,
+            className: 'select-checkbox',
+            targets: 0
+        }],
+        select: {
+            style: 'os',
+            selector: 'td:first-child'
+        },
+        order: [
+            [1, 'asc']
+        ],
+
+        dom: 'Bfrtip',
+        buttons: [
+            'selectAll',
+            'selectNone',
+        ],
+        select: true
+    });
+});
+//
+$(document).ready(function() {
+    $('#feesHistory').DataTable({
+        columnDefs: [{
+            orderable: false,
+            className: 'select-checkbox',
+            targets: 0
+        }],
+        select: {
+            style: 'os',
+            selector: 'td:first-child'
+        },
+        order: [
+            [1, 'asc']
+        ],
+
+        dom: 'Bfrtip',
+        buttons: [
+            'selectAll',
+            'selectNone',
+        ],
+        select: true
+    });
+});
+//
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+function tabDisplay(pageName, elmnt, color) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tab-btn");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+        tablinks[i].style.color = "";
+        tablinks[i].style.borderBottom = "";
+    }
+
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = color;
+    elmnt.style.color = '#F26419';
+    elmnt.style.borderBottom = '3px solid #F26419';
+
+
+}
+
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen2").click();
+
+function tabDisplay2(pageName, elmnt, color) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent2");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tab-btn2");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+        tablinks[i].style.color = "";
+    }
+
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = color;
+    elmnt.style.color = '#F26419';
+
+}
 
 
